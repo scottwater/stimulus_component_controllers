d6bb989cb191a3a1064c31fa9715c6ac670f6c79
Scott Watermasysk <scottwater@gmail.com>
2024-05-23 08:06:38 -0400

    Consistent Stimulus controller naming in app/components.
    This change allows us to name the default stimulus controllers for a component 'controller.js'

--- app/components/application_view_component.rb
+++ app/components/application_view_component.rb
@@ -1,3 +1,7 @@
 class ApplicationViewComponent < ViewComponentContrib::Base
   extend Dry::Initializer
+
+  def stimulus_controller
+    self.class.name.underscore.dasherize.tr("/", "-")
+  end
 end
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,8 +3,12 @@
 import { application } from "controllers/application"
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+// import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+// eagerLoadControllersFrom("controllers", application)
+
+import { eagerLoadControllersFrom } from "controllers/custom_loader"
 eagerLoadControllersFrom("controllers", application)
+
 
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
